04a1749c85531d69194e4d36b76002396a09aede
Mohamed Khaled <mohamedkhaledtharwat123@gmail.com>
2025-01-20 16:45:26 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -21,11 +22,37 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div>
+      {data.map((faq, index) => (
+        <AccordionItem
+          key={faq.title} 
+          num={index + 1}
+          title={faq.title}
+          text={faq.text}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen , setIsOpen] =useState(false);
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <div className={`item ${isOpen? "open"  :""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0 ${num}` :num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
